6df346b10dd674a677aa06bd8870ec545e506c31
ryasun85 <59563060+ryasun85@users.noreply.github.com>
2020-01-21 12:24:00 +0000

    Add files via upload

---
+++ activity1.py
@@ -0,0 +1,10 @@
+name = "Monika"
+age = 34
+fav_color = "green"
+
+print("My name is {}".format(name))
+print("I am {} years old".format(age))
+print("My favourite color is {}".format(fav_color))
+
+#oprion 2
+print("My favourite color is " + fav_color)
